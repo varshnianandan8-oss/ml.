5e46556150a95939adb60d06eb1487a8c13e331f
varshnianandan8-oss <varshnianandan8@gmail.com>
2026-03-22 11:39:50 +0530

    Add files via upload

---
+++ app.py.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+
+app = Flask(__name___)
+
+model = pickle.load(open("model.pkl","rb"))
+
+
+@ap.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict",methods=["POST"])
+def predict():
+    income = float(request.form["income"])
+    score = float(request.form["score"])
+
+    data = np.array([[income,score]])
+    result = model.predict(data)[0]
+ 
+    return render_template("result.html",prediction=result)
+
+if__name__ == "__main__";
+   app.run(debug=True)
+   
