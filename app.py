0fcecdd2e49dd94fce40b1d276eed7a8cc01b9c5
varshnianandan8-oss <varshnianandan8@gmail.com>
2026-03-22 13:56:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,7 @@ def predict():
  
     return render_template("index.html",prediction=result)
 
-   
+    if _name_ == "_main_":
+     import os
+     port = int(os.environ.get("PORT", 10000))
+     app.run(host="0.0.0.0", port=port)
